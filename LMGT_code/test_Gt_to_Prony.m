836718d9701f3cb49a7457e3780802005a51b575
Vladimir <Kolchuzhin@users.noreply.github.com>
2015-07-11 09:40:59 +0200

    Update test_Gt_to_Prony.m

--- LMGT_code/test_Gt_to_Prony.m
+++ LMGT_code/test_Gt_to_Prony.m
@@ -1,12 +1,9 @@
 % 20.02.2015
 % function test_Gt_to_Prony
 %-------------------------------------------------------------------------%
-
 t=logspace(-3,2)'; Gt=1+9*exp(-t.^0.5); 
 Nd=2;
 Gns=Gt_to_Prony(Gt,t,Nd);
-
-loglog(t,Gt,'-','LineWidth',2);
 
 
 tau=Gns(:,1); gn=Gns(:,2);
@@ -14,9 +11,12 @@ Ge=min(Gt)*0.98;
 X = exp(-kron(t,1./tau'));
 G_fit = Ge+X*gn;
 
+
+loglog(t,G_fit,'-','LineWidth',2);
 hold on;
-loglog(t,G_fit,'o','MarkerFaceColor','c');
-legend('data','Prony series');
+loglog(t,Gt,'o','MarkerFaceColor','c');
+
+legend('Prony series','data');
 grid on;
 
 xlabel('time'); 
